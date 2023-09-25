aa1b1657338333e957cf8a3c45fd8eca91999575
Kevin Bravo <kbravo1024@gmail.com>
2023-09-25 09:48:45 -0700

    add columns and password to employee

--- server/models.py
+++ server/models.py
@@ -1,5 +1,7 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
+
 
 from config import db, bcrypt
 
@@ -78,3 +80,18 @@ class Employee(db.Model, SerializerMixin):
     __tablename__ = 'employees'
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String, nullable = False)
+    username = db.Column(db.String, unique=True, nullable = False)
+    _password_hash = db.Column(db.String, nullable=False)
+
+    @hybrid_property
+    def password_hash(self):
+        return AttributeError()
+    
+    @password_hash.setter
+    def pasword_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
